af8f6280d77d94aa43935b5eb271544dec516824
jefer <jeferson.estudo2017@gmail.com>
2020-10-07 20:33:57 -0300

    Teste exCodBar

--- exCodBar/app/src/main/java/com/example/excodbar/MainActivity.java
+++ exCodBar/app/src/main/java/com/example/excodbar/MainActivity.java
@@ -1,45 +1,43 @@
+
 package com.example.excodbar;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.annotation.Nullable;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.os.Bundle;
-import android.provider.MediaStore;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
+        import android.content.Intent;
+        import android.graphics.Bitmap;
+        import android.os.Bundle;
+        import android.provider.MediaStore;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-Button btTiraFoto,btLeitura;
-ImageView imgSuaImage;
-EditText txLeitura;
+    Button btleitura,btTiraFoto;
+    ImageView imgSuaImagem;
+    EditText txLeitura;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lerCodigos();
         retiraFoto();
-       lerCodigos();
     }
-
-    private void lerCodigos() {
-        btTiraFoto =(Button)findViewById(R.id.btnLeitura);
-        txLeitura =(EditText)findViewById(R.id.edtLeitura);
-        btLeitura.setOnClickListener(new View.OnClickListener() {
+    private void lerCodigos(){
+        btleitura = (Button)findViewById(R.id.btnLeitura);
+        txLeitura = (EditText)findViewById(R.id.edtLeitura);
+        btleitura.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent abreCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(abreCamera, 0);
+                Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+                startActivityForResult(intent,0);
             }
         });
-
     }
-
     private void retiraFoto(){
         btTiraFoto =  findViewById(R.id.btnTiraFoto);
-        imgSuaImage = findViewById(R.id.suaImagem);
+        imgSuaImagem = findViewById(R.id.suaImagem);
 
         btTiraFoto.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,18 +47,18 @@ EditText txLeitura;
             }
         });
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 0){
-
+        if (requestCode==0){
+            txLeitura.setText(data.getStringExtra("SCAN_RESULT"));
         }
         Bundle bundle = data.getExtras();
         if (data!=null){
             Bitmap bitmap = (Bitmap)bundle.get("data");
-            imgSuaImage.setImageBitmap(bitmap);
+            imgSuaImagem.setImageBitmap(bitmap);
         }
     }
-
 
 }
